e742072bde4559bb5b472102ca1f28139dc33872
Andrey <asantiago.morales@um.es>
2025-11-10 21:26:58 +0100

    Estructura Alerta y notificacion
    He agregado la principal de alerta, notifiacion y la estructura de resources para los mismos, el txt es para que se guarde todo en git

---
+++ gestion-gastos/src/main/java/umu/tds/gestion_gastos/alerta/Notificacion.java
@@ -0,0 +1,17 @@
+package umu.tds.gestion_gastos.alerta;
+
+import java.time.LocalDate;
+
+public class Notificacion {
+	
+	private int id;
+	private String mensaje;
+	private LocalDate fecha;
+	private boolean isLeida;
+	
+	
+	public void marcarLeida() {
+		this.isLeida = true;
+	}
+
+}
---
+++ gestion-gastos/src/main/java/umu/tds/gestion_gastos/notificacion/Alerta.java
@@ -0,0 +1,16 @@
+package umu.tds.gestion_gastos.notificacion;
+
+import java.util.List;
+
+public class Alerta {
+	
+	private int id;
+	private double limite;
+	private boolean isActiva;
+	
+	
+	//public boolean ejecutarComprbar(List<Gasto> gastos) {
+		
+	//}
+
+}
